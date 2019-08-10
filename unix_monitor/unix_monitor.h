9c7261b00c5f16c826aeb68afe4a138c8b5d111b
Alex Rusu <aqrusu@gmail.com>
2019-08-10 14:11:50 +0300

    Fixed delimiter

--- unix_monitor/unix_monitor.h
+++ unix_monitor/unix_monitor.h
@@ -1,6 +1,6 @@
-//
-// Copyright (c) 2019 by Alexandru - George Rusu. All Rights Reserved.
-//
+/*
+ * Copyright (c) 2019 by Alexandru - George Rusu. All Rights Reserved.
+ */
 
 #ifndef _UNIX_MONITOR_H_
 #define _UNIX_MONITOR_H_
